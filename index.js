59ac013582135b38064bfe1ff611f0abdb1575d0
Qrymy <regist@qry.mx>
2022-02-16 18:38:49 +0900

    update script

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const main = () => {
+const handler = () => {
   const textarea = document.getElementById("merge_message_field");
 
   if (textarea) {
-    console.log("yes");
     textarea.value = "";
-  } else {
-    console.log("no");
   }
 };
 
-main();
+const observer = new MutationObserver(handler);
+
+observer.observe(document, { childList: true, subtree: true });
